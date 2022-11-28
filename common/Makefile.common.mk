e363b5cabae68d15c50664c068334e66fc4d3fef
xcliu-ca <32179551+xcliu-ca@users.noreply.github.com>
2022-11-28 17:01:46 -0500

    new prow cluster for ltsr release (#195)
    * new prow cluster for ltsr release
    
    * Update markdown
    
    lint tailing space

--- common/Makefile.common.mk
+++ common/Makefile.common.mk
@@ -24,8 +24,8 @@ INSTALL_HOOKS := $(shell find .git/hooks -type l -exec rm {} \; && \
 # GKE section
 ############################################################
 PROJECT ?= oceanic-guard-191815
-ZONE    ?= us-west1-a
-CLUSTER ?= prow
+ZONE    ?= us-east5-c
+CLUSTER ?= bedrock-prow
 tt3=$(shell which 2to3)
 
 activate-serviceaccount:
