334001093334b71e4246f75786c4643b3aac9c49
PRTamilanban <75234895+PRTamilanban@users.noreply.github.com>
2023-08-31 12:04:23 +0530

    whitesrc fix for CVE-2020-11023 & CVE-2020-11022 (#219)

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -1,4 +1,3 @@
-//
 // Copyright 2020 IBM Corporation
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
 package main
 
 import (
@@ -26,7 +24,7 @@ import (
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	"k8s.io/client-go/rest"
 
-	certmgr "github.com/jetstack/cert-manager/pkg/apis/certmanager/v1alpha1"
+	certmgr "github.com/ibm/ibm-cert-manager-operator/apis/certmanager/v1alpha1"
 
 	"github.com/IBM/ibm-monitoring-grafana-operator/pkg/apis"
 	"github.com/IBM/ibm-monitoring-grafana-operator/pkg/controller"
